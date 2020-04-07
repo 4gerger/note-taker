d6fd89f6bb141fa2691a682b0d3e165438698df3
4gerger <gzhlam@gmail.com>
2020-04-07 23:10:54 +1000

    added get post delete routes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# note-taker
+# Note-Taker
+
+Author:
+- Lam, Gerald
+
+## Description
+
+This is a notes application which is used to write, save, and delete notes. The app will use an express backend and save and retrieve note data from a JSON file.
+
+Please see below for the application's screenshot:
+![screenshot](public/assets/img/screenshot1.png)
+![screenshot](public/assets/img/screenshot2.png)
+![screenshot](public/assets/img/screenshot3.png)
+
+## Usage
+
+Go to the following link: 
+
+
+
+- Click on "Get Started" to get to the notes page
+- Type in the fields "Note Title" and "Note Text"
+- Click on the "Save" icon on the top righthand corner
+- To delete the note, click on the "Delete/Bin" icon next to the "Note Title"
+
+Try it out!
+
+## Libraries and Frameworks
+
+<b>Built with<b>
+
+- NodeJS
+- Express.js
+- Bootstrap
+- CSS
+
+## Roadmap
+
+- Additional CSS styling can be added to the application
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,57 @@
+// ================================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ================================================================================
+
+var fs = require("fs");
+var activeNote = require("../db/db.json")
+
+
 module.exports = function (app) {
+
+    // API GET Requests
+    // Retrieve data from db.json when user "visits" the page
+
+    app.get("/api/notes", function (req, res) {
+        res.json(activeNote);
+    })
+
+    // API POST Requests
+    // When user clicks the "save" icon, the server saves the data to db.json
+
+    app.post("/api/notes", function (req, res) {
+
+        var newNote = req.body;
+        id = activeNote.length + 1;
+        newNote.id = id.toString();
+
+        activeNote.push(newNote);
+        res.json(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(activeNote), function (err) {
+            if (err) throw err;
+            console.log("Data has been added from database")
+        })
+    });
+
+    // API DELETE Requests
+    // When user clicks the "delete" icon, the note is selected deleted from db.json
+
+    app.delete("/api/notes/:character", function (req, res) {
+        var selected = req.params.character;
+
+        for (var i = 0; i < activeNote.length; i++) {
+            if (selected === activeNote[i].id) {
+                activeNote.splice(i, 1);
+
+                fs.writeFile("./db/db.json", JSON.stringify(activeNote), function (err) {
+                    if (err) throw err;
+                    console.log("Data has been deleted from database");
+                })
+                return res.json(activeNote);
+            }
+        }
+        return res.json(false);
+    })
 
 }
